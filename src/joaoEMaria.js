f84a621e0742713fb50485553b0f6ed92abaeab8
ItaloFSilver <fagundes.italo@estudante.ufjf.br>
2025-02-25 23:00:36 -0300

    .

---
+++ src/joaoEMaria.js
@@ -0,0 +1,25 @@
+console.log('módulo joaoEMaria');
+
+let maria=2;
+let joao=1;
+function getMaria(){
+    return maria;
+}
+function getJoao(){
+    return joao;
+}
+
+function setJoao(novoValor){
+    if (novoValor>0)
+        joao=novoValor;
+    else
+        joao=0;
+}
+function setMaria(novoValor){
+    if (novoValor>0)
+        maria=novoValor;
+    else
+        maria=0;
+}
+
+export{getMaria, getJoao, setJoao, setMaria};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { getJoao, getMaria } from "./joaoEMaria.js";
+console.log('módulo main');
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maria tem ', getMaria(), ' maçãs!');
